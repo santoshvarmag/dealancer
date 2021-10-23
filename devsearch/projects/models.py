a48cf52b29bb68b76e5e911c851abc138c3b7d4f
santosh <santoshg.seo@gmail.com>
2021-10-23 15:41:34 +0530

    Add project model to database

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=255, null=True, blank=True)
+    source_link = models.CharField(max_length=255, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+
