87bfcb07fca85c346aef5134483b15d3fccd749f
santosh <santoshg.seo@gmail.com>
2021-10-24 09:07:39 +0530

    Add urls to create, update and delete

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>/', views.single_project, name='project'),
+    path('create-project/', views.createProject, name="create-project"),
+    path('update/<str:pk>/', views.editProject, name='update-project'),
+    path('delete/<str:pk>/', views.deleteProject, name="delete-project"),
 ]
