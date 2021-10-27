fb53d3b9777f03205704d907c56d7ed2d015a41f
santosh <santoshg.seo@gmail.com>
2021-10-27 11:03:03 +0530

    Add profile detail page URL

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profiles, name="profiles")
+    path('', views.profiles, name="profiles"),
+    path('dev/<str:pk>/', views.devProfile, name='dev-profile' )
 ]
 
