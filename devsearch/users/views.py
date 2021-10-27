cf1d30ecde611fe0133de15cb9d41eaf586db104
santosh <santoshg.seo@gmail.com>
2021-10-27 11:03:55 +0530

    Add profiles data and profile detail page

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Profile
+from projects.models import Project
 
 # Create your views here.
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    devs = Profile.objects.all()
+    context = {'devs':devs}
+    return render(request, 'users/profiles.html', context)
+
+def devProfile(request, pk):
+    dev = Profile.objects.get(id=pk)
+    projects = Project.objects.filter(owner=dev)
+    context = {'dev':dev, 'projects':projects}
+    return render(request, 'users/profile.html', context)
