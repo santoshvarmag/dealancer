1cbd59b2bc82926e26e86535d50a6e2f8134ae67
santosh <santoshg.seo@gmail.com>
2021-10-31 17:11:18 +0530

    Add path to login and logout

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('', views.profiles, name="profiles"),
     path('dev/<str:pk>/', views.devProfile, name='dev-profile' )
 ]
