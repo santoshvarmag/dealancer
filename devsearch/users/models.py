7968a9f65070ccb0300692c2577bb2bb98aec37a
santosh <santoshg.seo@gmail.com>
2021-10-31 10:15:06 +0530

    Add Skill model and location table to profile model

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+from django.db.models.base import Model
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
     username = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
     short_intro = models.CharField(max_length=255, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(
@@ -26,3 +29,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
