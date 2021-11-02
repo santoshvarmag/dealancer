153efae3ec5ce6b4c5b2a944573c63e68d749758
santosh <santoshg.seo@gmail.com>
2021-11-02 18:20:20 +0530

    Add register functionality

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def profiles(request):
@@ -17,6 +18,7 @@ def devProfile(request, pk):
     return render(request, 'users/profile.html', context)
 
 def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -37,9 +39,28 @@ def loginUser(request):
         else:
             messages.warning(request, "Username or Password incorrect")
 
-    return render(request, 'users/login_register.html')
+    return render(request, 'users/login_register.html', {'page':page})
 
 def logoutUser(request):
     logout(request)
     messages.success(request, "User was sucessfully logged out!")
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, "User account was created!")
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.warning(request, "An error has occured during the registration")            
+    context = {'page':page, 'form':form}
+    return render(request, 'users/login_register.html', context)
