8ccf4372ca12f10ba9ec93f0b019d790cb63adc4
santosh <santoshg.seo@gmail.com>
2021-11-05 15:33:48 +0530

    Add acount path to users url file

--- devsearch/users/urls.py
+++ devsearch/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
     path('', views.profiles, name="profiles"),
-    path('dev/<str:pk>/', views.devProfile, name='dev-profile' )
+    path('dev/<str:pk>/', views.devProfile, name='dev-profile' ),
+    path('account/', views.userAccount, name="account"),
 ]
 
