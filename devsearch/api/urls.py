14204752d7f0c3c3dd4db8c2f064bbdc1849ae6e
santosh <santoshg.seo@gmail.com>
2022-01-03 16:22:22 +0530

    Added Token URL for REST

--- devsearch/api/urls.py
+++ devsearch/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from api import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   
     path('', views.getRoutes),
     path('projects/', views.getProjects),
     path('project/<str:pk>/', views.getProject),
+    path('project/<str:pk>/vote/', views.projectVote),
 ]
