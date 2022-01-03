0e47a8943560eb1d66f141df862e9e86af649a5e
santosh <santoshg.seo@gmail.com>
2022-01-03 16:22:03 +0530

    Added vote func from REST

--- devsearch/api/views.py
+++ devsearch/api/views.py
@@ -1,26 +1,29 @@
-from rest_framework import serializers
-from rest_framework.decorators import api_view
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 
 from .serializers import ProjectSerializer
 
-from projects.models import Project 
+from projects.models import Project, Review
 
 
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        {'GET':'/api/projects'},
-        {'GET':'/api/projects/id'},
-        {'POST':'/api/projects/id/vote'},
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
 
-        {'POST':'/api/users/token'},
-        {'POST':'/api/users/token/refresh'},
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
     ]
     return Response(routes)
 
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
@@ -30,5 +33,23 @@ def getProjects(request):
 @api_view(['GET'])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project
+    )
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
