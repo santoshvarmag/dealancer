d4dec79078f43de3b76df28752c804dd90c1ab47
santosh <santoshg.seo@gmail.com>
2022-01-04 15:44:01 +0530

    Issue - If profile deleted, project list gives error

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -6,29 +6,31 @@ from users.models import Profile
 
 # Create your models here.
 class Project(models.Model):
-    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    featured_image = models.ImageField(default='default.jpg',null=True, blank=True)
+    featured_image = models.ImageField(
+        default='default.jpg', null=True, blank=True)
     demo_link = models.CharField(max_length=255, null=True, blank=True)
     source_link = models.CharField(max_length=255, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
     votes_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ['-vote_ratio', '-votes_total', 'title']
-    
+
     @property
     def reviewers(self):
         queryset = self.review_set.all().values_list('owner__id', flat=True)
         return queryset
-
 
     @property
     def getVoteCount(self):
@@ -52,8 +54,9 @@ class Review(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=255, choices=VOTE_TYPE)
-    created =  models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     class Meta:
         unique_together = [['owner', 'project']]
@@ -63,10 +66,10 @@ class Review(models.Model):
 
 
 class Tag(models.Model):
-    name =  models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.name
-        
